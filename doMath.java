ce7ba7fb671a9ac8c94a5f91297de33d18146c5c
codefox96 <58374929+codefox96@users.noreply.github.com>
2020-01-19 12:45:46 -0500

    Comments

--- doMath.java
+++ doMath.java
@@ -2,19 +2,19 @@ package beginTrials;
 import static java.lang.System.out;
 
 public class doMath {
-    //Initialization of int variable
+    //Creation of int variable
     public static int number;
-
+    
+    //Creation of Constructor
     public doMath(int given) {
         this.number = given;
     }
-
-    //
+    //Method to be inherited
     public int useNumber() {
         int use = number * number;
         return use;
     }
-
+    //Prints to user
     public void display() {
         out.printf("The square of %s is %s\n",number,useNumber());
         out.print("Trial 2 Complete\n");
